3aba4251387a2b66e0fca3773c498ecafd3e31a7
chahuigwang <chahuigwang@gmail.com>
2024-11-07 01:14:34 +0900

    Update PlaceData.java

--- src/main/java/com/haandy/travel_planner_server/domain/placeData/data/PlaceData.java
+++ src/main/java/com/haandy/travel_planner_server/domain/placeData/data/PlaceData.java
@@ -1,5 +1,6 @@
 package com.haandy.travel_planner_server.domain.placeData.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +8,12 @@ import lombok.Setter;
 @Setter
 public class PlaceData {
     private String name;
+
+    @JsonProperty("photo")
     private String[] photo;
     private String address;
+
+    @JsonProperty("open_hours")
     private String[] open_hours;
     private String admission_fee;
     private String web_site;
